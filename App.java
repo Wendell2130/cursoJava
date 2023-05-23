b819c1f58ffcd0de498353e3f671c6f3cf545458
Wendell Delmondes <wendell2009@hotmail.com>
2023-05-23 20:08:55 -0300

    lendo datas e horas e mostrando com LocalDateTime

--- App.java
+++ App.java
@@ -1,7 +1,7 @@
 package dataEhora;
 
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.Scanner;
@@ -11,22 +11,27 @@ public class App {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc=new Scanner(System.in);
+		
+		
 		System.out.println("Sistema de Ponto");
 		System.out.println("Digite hora de entrada(HH:MM):");
 		String entryTime=sc.nextLine();
 		System.out.println("Digite data de entrada(DD/MM/AAAA):");
 		String entryDate=sc.nextLine();
+		String dateTime=entryDate+":"+entryTime;
+		DateTimeFormatter form=DateTimeFormatter.ofPattern("dd/MM/yyyy:kk:mm");
+		
+		LocalDateTime entry=LocalDateTime.parse(dateTime,form);
 		
 		System.out.println("Digite hora de saida(HH:MM):");
 		String exitTime=sc.nextLine();
 		System.out.println("Digite data de entrada(DD/MM/AAAA):");
 		String exitDate=sc.nextLine();
+		dateTime=exitDate+":"+exitTime;
+		LocalDateTime exit=LocalDateTime.parse(dateTime,form);
 		
-		
-		System.out.println(entryTime);
-		System.out.println(entryDate);
-		System.out.println(exitTime);
-		System.out.println(exitDate);
+		System.out.println(entry);
+		System.out.println(exit);
 		
 	}
 
